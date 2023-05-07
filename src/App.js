9166d46f3ac1449012b2f270ca03025b0ba7830e
faseehahmed1 <faseeh_455@hotmail.com>
2023-05-07 17:01:32 +0100

    main page view done

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import classes from "./App.module.css";
 import Navbar from "./Components/Navbar/Navbar";
 import MENU_ITEMS from "./Data/data";
 import CallToAction from "./Components/CallToAction/CallToAction";
+import Menu from "./Components/Menu/Menu";
 
 function App() {
   return (
     <div>
-        <Navbar />
+      <Navbar />
       <div className={classes.container_centre}>
-        <CallToAction/>
+        <CallToAction />
+        <div className={classes.container_menu}>
+          {MENU_ITEMS.map((item) => {
+            return <Menu item={item} key={item.id}></Menu>;
+          })}
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
+
+// id: number;
+// dish: string;
+// description: string;
+// cost: number;
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,5 +1,23 @@
 import classes from "./Menu.module.css";
 
-const Menu = () => {};
+const Menu = ({ item }) => {
+  const { dish, description, cost } = item;
+  return (
+    <div className={classes.container}>
+      <div className={classes.containerLHS}>
+        <h3>{dish}</h3>
+        <p>{description}</p>
+        <p>$ {cost}</p>
+      </div>
+      <div className={classes.containerRHS}>
+        <div className={classes.containerRHS_amount}>
+          <p>Amount</p>
+          <input type="number" value={1}></input>
+        </div>
+        <button>+ Add</button>
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
